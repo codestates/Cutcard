2074908ac141d1ddb51165c4102b4c1b466bc09a
Ohney <yhu633@gmail.com>
2021-12-23 18:11:01 +0900

    Seeder modified

--- server/seeders/20211215133735-demo-users.js
+++ server/seeders/20211215133735-demo-users.js
@@ -4,26 +4,6 @@ const { test } = require('../config/config');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const userId = await queryInterface.bulkInsert(
-      'users',
-      [
-        {
-          email: 'test@test.com',
-          password: '1234',
-          nickname: 'cutCard',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          email: 'yechan@gmail.com',
-          password: '1q2w3e4r',
-          nickname: '예찬',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      { returning: ['id'] }
-    );
     const cardId = await queryInterface.bulkInsert(
       'cards',
       [
@@ -84,82 +64,8 @@ module.exports = {
           updatedAt: new Date(),
         },
       ],
-      { returning: ['id'] }
+      { returning: ['id'] },
     );
-    const userCardId = await queryInterface.bulkInsert(
-      'userCards',
-      [
-        {
-          isCut: true,
-          remainValue: 400000,
-          repaymentDay: 25,
-          userId: 1,
-          cardId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          isCut: true,
-          remainValue: 400000,
-          repaymentDay: 25,
-          userId: 2,
-          cardId: 2,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          isCut: true,
-          remainValue: 400000,
-          repaymentDay: 25,
-          userId: 2,
-          cardId: 3,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      { returning: ['id'] }
-    );
-    await queryInterface.bulkInsert('transactions', [
-      {
-        year: 2021,
-        month: 12,
-        day: 17,
-        category: '월급',
-        price: 2500000,
-        isIncome: true,
-        outcomeIsCash: false,
-        userId: 1,
-        userCardId: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        year: 2021,
-        month: 12,
-        day: 17,
-        category: '식비',
-        price: 50000,
-        isIncome: false,
-        outcomeIsCash: false,
-        userId: 2,
-        userCardId: 2,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        year: 2021,
-        month: 12,
-        day: 17,
-        category: '생활용품',
-        price: 25000,
-        isIncome: false,
-        outcomeIsCash: true,
-        userId: 2,
-        userCardId: 3,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
